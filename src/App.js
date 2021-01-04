a8666149a9463a3c47bc3af3d20b6c70ffd37b1d
Sreejit7 <sreejitde7@gmail.com>
2021-01-05 03:27:36 +0530

    Added custom search option

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import Header from './Header';
 function App() {
   const [newsList, setNewsList] = useState([]);
   const [input, setInput] = useState("");
+  const [inputOn, setInputOn] = useState(false);
   const [isToggled, setIsToggled] = useState(false);
   //const currentDate = new Date().toISOString();
   //console.log(currentDate);
+  
+  const changeInput = (e) => {
+    setInput(e.target.value);
+    if(input !== "")
+      setInputOn(true);
+    else
+      setInputOn(false);
+  };
+  
+
   useEffect(() => {
-    const url = 'https://gnews.io/api/v4/top-headlines?lang=en&sortby=publishedAt&token=f556748f6689e5c7a5c0444335cded03';
+    const url = inputOn? `https://gnews.io/api/v4/search?q=${input}&lang=en&sortby=publishedAt&token=f556748f6689e5c7a5c0444335cded03` :'https://gnews.io/api/v4/top-headlines?lang=en&sortby=publishedAt&token=f556748f6689e5c7a5c0444335cded03';
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -18,12 +29,13 @@ function App() {
       setNewsList(data.articles);
     })
     .catch((err) => console.log(err.message));
-  }, []);
+  }, [input]);
   //console.log(newsList);
   return (
     <div className = "app">
       <Header toggle = {isToggled}/>
       <div className = {`app__body ${isToggled && "app-dark"}`}>
+        <input className = "news__input" placeholder = "Looking for something specific?" value = {input} onChange = {changeInput}/>
         <div className="app__switch">
           <Switch isToggled = {isToggled} onToggle = {() => setIsToggled(!isToggled)}/> 
         </div>
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css';
 function Header({toggle}) {
+  
   return (
     <div className = {`header ${toggle && "header__toggle"}`}>
       <h1>HEADLINER</h1>
