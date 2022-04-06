483566af29d3f0853b3bd36cb31f9a1cb43bae4e
Sreejit7 <sreejitde7@gmail.com>
2022-04-06 13:58:39 +0530

    upgrade to react-18 🥳

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
     </Routes>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
