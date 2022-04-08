c5f609be09e964af2db32ef407c77a7b3e890979
Sreejit7 <sreejitde7@gmail.com>
2022-04-08 23:50:30 +0530

    Reset the search input on topic change; only dispatch topic change if a different topic is selected

--- src/components/TopicCard/index.jsx
+++ src/components/TopicCard/index.jsx
@@ -5,24 +5,38 @@ import {
 } from "../../contexts/useSearchContext";
 
 const TopicCard = ({ topic, active }) => {
-  const { dispatch } = useSearchContext();
+  const {
+    state: { topic: currentTopic },
+    dispatch,
+  } = useSearchContext();
 
   const topicSelectHandler = () => {
-    dispatch({
-      type: SearchActionTypes.SELECT_TOPIC,
-      topic: topic.key,
-    });
+    if (topic !== currentTopic) {
+      dispatch({
+        type: SearchActionTypes.SELECT_TOPIC,
+        topic: topic.key,
+      });
+    }
   };
 
   return (
-    <li className="relative h-40 cursor-pointer" data-testid="card" onClick={topicSelectHandler}>
+    <li
+      className="relative h-40 cursor-pointer"
+      data-testid="card"
+      onClick={topicSelectHandler}
+    >
       <div
         className="absolute h-40 w-full bg-center bg-no-repeat bg-cover inline-block rounded-lg"
         style={{ backgroundImage: `url(${topic.image})` }}
       />
       <div className="absolute h-40 w-full rounded-lg backdrop-blur-sm" />
       <div className="absolute h-40 w-full rounded-lg bg-gradient-to-b opacity-50 from-gray-600 via-gray-700 to-black" />
-      <div data-testid="active-ring" className={`absolute h-40 w-full rounded-lg transition duration-700 shadow-md ${active && "ring-2 ring-red-600"}`} />
+      <div
+        data-testid="active-ring"
+        className={`absolute h-40 w-full rounded-lg transition duration-700 shadow-md ${
+          active && "ring-2 ring-red-600"
+        }`}
+      />
       <article className="absolute h-full w-full transition duration-700 text-white flex items-center justify-center">
         <h2 className="font-bold text-xl">{topic.text}</h2>
       </article>
